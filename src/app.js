947bee102fe545cf078a99ff7e3782f1a4d64441
Zekumoru Dragonhart <dev@zekumoru.com>
2022-12-09 23:34:33 +0100

    Extract renderBoard to DOMUtils.js

--- src/app.js
+++ src/app.js
@@ -1,33 +1,7 @@
 import '@mdi/font/css/materialdesignicons.css';
 import 'normalize.css';
 import './style.css';
-import { BOARD_SIZE } from './scripts/GameBoard';
+import { renderBoard } from './scripts/DOMUtils';
 
 renderBoard(document.querySelector('#user-game-board'));
 renderBoard(document.querySelector('#enemy-game-board'));
-
-function renderBoard(gameBoard) {
-  for (let i = 0; i < (BOARD_SIZE + 1); i++) {
-    for (let j = 0; j < (BOARD_SIZE + 1); j++) {
-      const cell = createCell(j, i);
-      gameBoard.appendChild(cell);
-    }
-  }
-}
-
-function createCell(x, y) {
-  const cell = document.createElement('div');
-  cell.classList.add('cell');
-
-  if (!(x === 0 || y === 0)) cell.classList.add('game-cell');
-  else if (x !== 0 && y === 0) {
-    cell.innerHTML = `<div class="content">${String.fromCharCode(x + 64)}</div>`;
-    cell.classList.add('alpha-label');
-  }
-  else if (x === 0 && y !== 0) {
-    cell.innerHTML = `<div class="content">${y}</div>`;
-    cell.classList.add('numeric-label');
-  }
-
-  return cell;
-}
---
+++ src/scripts/DOMUtils.js
@@ -0,0 +1,31 @@
+import { BOARD_SIZE } from './GameBoard';
+
+export default {
+  renderBoard,
+};
+
+export function renderBoard(gameBoard) {
+  for (let i = 0; i < (BOARD_SIZE + 1); i++) {
+    for (let j = 0; j < (BOARD_SIZE + 1); j++) {
+      const cell = createCell(j, i);
+      gameBoard.appendChild(cell);
+    }
+  }
+}
+
+function createCell(x, y) {
+  const cell = document.createElement('div');
+  cell.classList.add('cell');
+
+  if (!(x === 0 || y === 0)) cell.classList.add('game-cell');
+  else if (x !== 0 && y === 0) {
+    cell.innerHTML = `<div class="content">${String.fromCharCode(x + 64)}</div>`;
+    cell.classList.add('alpha-label');
+  }
+  else if (x === 0 && y !== 0) {
+    cell.innerHTML = `<div class="content">${y}</div>`;
+    cell.classList.add('numeric-label');
+  }
+
+  return cell;
+}
