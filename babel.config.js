45b284006ad6ab051b48b9a63f004ad6f75e2ccc
Zekumoru Dragonhart <dev@zekumoru.com>
2022-12-09 12:57:02 +0100

    Fix wrong jest webpack configuration

---
+++ babel.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  presets: [['@babel/preset-env', { targets: { node: 'current' } }]],
+  moduleFileExtensions: ['js', 'jsx'],
+  moduleDirectories: ['node_modules', 'bower_components', 'shared'],
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/__mocks__/fileMock.js',
+    '\\.(css|less)$': '<rootDir>/__mocks__/styleMock.js',
+  },
+};
