2c7a2903101a8e55c825e8d913c11897e9ba69f0
Zekumoru Dragonhart <dev@zekumoru.com>
2022-12-09 21:58:38 +0100

    Create game board html

--- src/app.js
+++ src/app.js
@@ -1,3 +1,13 @@
 import '@mdi/font/css/materialdesignicons.css';
 import 'normalize.css';
 import './style.css';
+import { BOARD_SIZE } from './scripts/GameBoard';
+
+const gameBoard = document.querySelector('#game-board');
+for (let i = 0; i < BOARD_SIZE; i++) {
+  for (let j = 0; j < BOARD_SIZE; j++) {
+    const cell = document.createElement('div');
+    cell.className = 'cell';
+    gameBoard.appendChild(cell);
+  }
+}
