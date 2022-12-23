ec3197c31b1047f9250aea66995f76e69e86ed30
Zekumoru Dragonhart <dev@zekumoru.com>
2022-12-23 18:52:21 +0100

    Update app.js for the reworked UI

--- src/app.js
+++ src/app.js
@@ -1,5 +1,3 @@
-/* eslint-disable no-constant-condition, no-loop-func, no-await-in-loop, no-console */
-import '@mdi/font/css/materialdesignicons.css';
 import 'normalize.css';
 import './style.css';
 import {
@@ -7,43 +5,37 @@ import {
 } from './scripts/DOMUtils';
 import Game from './scripts/Game';
 import Player from './scripts/Player';
-import ComputerPlayer from './scripts/ComputerPlayer';
 
 const userBoardDOM = renderBoard(document.querySelector('#user-game-board'));
-const enemyBoardDOM = renderBoard(document.querySelector('#enemy-game-board'));
 
-enemyBoardDOM.addEventListener('click', (e) => {
-  if (!(e.target.classList.contains('game-cell') || e.target.classList.contains('ship-block'))) return;
+// enemyBoardDOM.addEventListener('click', (e) => {
+//   if (!(e.target.classList.contains('game-cell') || e.target.classList.contains('ship-block'))) return;
 
-  Game.getUserInput({
-    board: enemyBoardDOM,
-    ...e.target.dataset,
-  });
-});
+//   Game.getUserInput({
+//     board: enemyBoardDOM,
+//     ...e.target.dataset,
+//   });
+// });
 
 const player = new Player('Player');
-const computer = new ComputerPlayer();
 
 window.addEventListener('resize', () => {
   renderShips(player.board, userBoardDOM);
-  renderShips(computer.board, enemyBoardDOM, true);
 });
 
 Game.populateBoard(player.board);
-Game.populateBoard(computer.board);
 
 renderShips(player.board, userBoardDOM);
-renderShips(computer.board, enemyBoardDOM, true);
 
 makeShipsDraggable(player.board, userBoardDOM);
 makeShipsRotatable(player.board, userBoardDOM);
 makeDroppable();
 
-Game.start({
-  playerOne: player,
-  playerTwo: computer,
-  playerOneDOM: userBoardDOM,
-  playerTwoDOM: enemyBoardDOM,
-}).then((winner) => {
-  console.log(`${winner.name} wins!`);
-});
+// Game.start({
+//   playerOne: player,
+//   playerTwo: computer,
+//   playerOneDOM: userBoardDOM,
+//   playerTwoDOM: enemyBoardDOM,
+// }).then((winner) => {
+//   console.log(`${winner.name} wins!`);
+// });
