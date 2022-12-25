58415ce9e8ca32f00498ab0f1873fe7872c2513e
Zekumoru Dragonhart <dev@zekumoru.com>
2022-12-25 01:24:03 +0100

    Show fleet

--- src/app.js
+++ src/app.js
@@ -6,7 +6,9 @@ import {
 import Game from './scripts/Game';
 import Player from './scripts/Player';
 
-const userBoardDOM = renderBoard(document.querySelector('#user-game-board'));
+const mainBoardDOM = renderBoard(document.querySelector('#main-game-board'));
+const mainLabel = document.querySelector('.main-label');
+const tipParagraph = document.querySelector('.tip');
 
 // enemyBoardDOM.addEventListener('click', (e) => {
 //   if (!(e.target.classList.contains('game-cell') || e.target.classList.contains('ship-block'))) return;
@@ -20,20 +22,49 @@ const userBoardDOM = renderBoard(document.querySelector('#user-game-board'));
 const player = new Player('Player');
 
 window.addEventListener('resize', () => {
-  renderShips(player.board, userBoardDOM);
+  renderShips(player.board, mainBoardDOM);
 });
 
 Game.populateBoard(player.board);
 
-renderShips(player.board, userBoardDOM);
+renderShips(player.board, mainBoardDOM);
 
-makeShipsDraggable(player.board, userBoardDOM);
-makeShipsRotatable(player.board, userBoardDOM);
+makeShipsDraggable(player.board, mainBoardDOM);
+makeShipsRotatable(player.board, mainBoardDOM);
 makeDroppable();
 
-document.querySelector('#start-game-btn').addEventListener('click', () => {
+const startGameButton = document.querySelector('#start-game-btn');
+startGameButton.addEventListener('click', (e) => {
   removeInteracts(['ship', 'ship-block', 'game-cell']);
+
+  e.target.remove();
+  tipParagraph.remove();
+  mainLabel.textContent = 'Your Turn';
+
+  // add fleet indicators
+  const mainFleetDOM = document.querySelector('#main-board-fleet');
+  const shipSize = mainBoardDOM.clientWidth / 11;
+
+  player.board.ships.forEach((ship) => {
+    const shipElement = document.createElement('div');
+    shipElement.className = 'ship';
+    shipElement.style.width = `${shipSize * ship.length}px`;
+    shipElement.style.height = `${shipSize}px`;
+
+    for (let i = 0; i < ship.length; i++) {
+      const shipBlock = document.createElement('div');
+      shipBlock.className = 'ship-block';
+
+      shipElement.appendChild(shipBlock);
+    }
+
+    mainFleetDOM.insertAdjacentElement('beforeend', shipElement);
+  });
+
+  renderShips(player.board, mainBoardDOM);
 });
+
+startGameButton.click();
 
 // Game.start({
 //   playerOne: player,
